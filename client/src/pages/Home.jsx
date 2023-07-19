6b2e56fd542763af537cd46f80fd2d812041b42f
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-19 16:48:50 -0700

    will start customizer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,11 +8,54 @@ import {
     slideAnimation
 } from'../config/motion';
 import state from '../store';
+import { CustomButton } from '../components';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const snap = useSnapshot(state);
+
+    return (
+        <AnimatePresence>
+            {snap.intro && (
+                <motion.section 
+                    className='home'
+                    {...slideAnimation('left')}
+                >
+                    <motion.header {...slideAnimation('down')}>
+                        <img
+                            src='./threejs.png'
+                            alt='logo'
+                            className='w-8 h-8 object-contain'
+                        />
+                    </motion.header>
+
+                    <motion.div 
+                        className='home-content'
+                        {...headContainerAnimation}
+                    >
+                        <motion.div {...headTextAnimation} className='flex flex-col gap-5'>
+                            <h1 className='head-text'>
+                                LET'S <br className='xl:block hidden'/> DO IT.
+                            </h1>
+                        </motion.div>
+                        <motion.div>
+                            <p className='max-w-md font-normal text-gray-600 text-base'>
+                                Create your unique and exclusive shirt with our brand-new
+                                 3D customization tool. <strong>Unleash your imagination</strong>
+                                 {" "} and define your own style.
+                            </p>
+
+                            <CustomButton
+                                type='filled'
+                                title='Customize It'
+                                handleClick={() => state.intro = false}
+                                customStyles='w-fit mt-4 px-4 py-2.5 font-bold text-sm'
+                            />
+                        </motion.div>
+                    </motion.div>
+                </motion.section>
+            )}
+        </AnimatePresence>
+    )
 }
 
 export default Home
