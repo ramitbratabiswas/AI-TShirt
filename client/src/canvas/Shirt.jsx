df42de2967410c4a42f4fdd31bd3b5a214985463
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-19 18:34:06 -0700

    Completed Shirt!

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -14,15 +14,40 @@ const Shirt = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal);
 
+  useFrame((state, delta) => (
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  ))
+
+  const stateString = JSON.stringify(snap);
+
   return (
-    <group>
+    <group key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         dispose={null}
       >
-        
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.2}
+            map={logoTexture}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+
       </mesh>
     </group>
   )
