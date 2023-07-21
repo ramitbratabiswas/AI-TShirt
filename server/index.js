1dd660f76bdba03d4423396f376e55a07f84c22d
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2023-07-21 07:22:25 -0700

    server set up and working

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'hewwo'});
+  res.status(200).json({ message: 'hewwo' });
 })
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(8080, () => console.log('Server started on port 8080'));
